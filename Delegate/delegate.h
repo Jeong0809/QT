50b39f7c028dff1d93d334fd9c520176a9fb0f0f
Jeong0809 <115696581+Jeong0809@users.noreply.github.com>
2022-11-08 12:24:11 +0900

    1108

---
+++ Delegate/delegate.h
@@ -0,0 +1,24 @@
+#ifndef DELEGATE_H
+#define DELEGATE_H
+
+#include <QItemDelegate>
+
+class Delegate : public QItemDelegate
+{
+public:
+    explicit Delegate(QObject *parent = nullptr);
+
+    QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option,
+                          const QModelIndex& index) const;
+    void setEditorData(QWidget* editor, const QModelIndex &index) const;
+    void setModelData(QWidget* editor, QAbstractItemModel* model,
+                      const QModelIndex& index) const;
+    void updateEditorGeometry(QWidget* editor, const QStyleOptionViewItem& option,
+                              const QModelIndex& index) const;
+
+
+signals:
+
+};
+
+#endif // DELEGATE_H
---
+++ MVC/widget.cpp
@@ -0,0 +1,34 @@
+#include "widget.h"
+#include "personmodel.h"
+
+
+#include <QListView>
+#include <QTableView>
+#include <QTreeView>
+#include <QVBoxLayout>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    PersonModel * model = new PersonModel(this);
+
+    QListView* listView = new QListView(this);
+    listView->setModel(model);
+
+    QTableView* tableView = new QTableView(this);
+    tableView->setModel(model);
+
+    QTreeView* treeView = new QTreeView(this);
+    treeView->setModel(model);
+    treeView->setRootIsDecorated(false);
+
+    QVBoxLayout* layout = new QVBoxLayout(this);
+    layout->addWidget(listView);
+    layout->addWidget(tableView);
+    layout->addWidget(treeView);
+}
+
+Widget::~Widget()
+{
+}
+
